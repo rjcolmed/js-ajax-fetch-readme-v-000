ba402c796ffd7525fb825bf85f9a38e448c8d71e
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-27 12:34:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+const token = '855e9d7775c30c0d79516d236d5bddc62f24da13'
+fetch('https://api.github.com/rjcolmed/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+.then(res => res.json())
+.then(json => console.log(json));
